c8a39b0fb3ce77d6a2487f6e3b4183ebd698159e
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-04 17:22:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+      
+        int x = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 1; i <= x; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
+
